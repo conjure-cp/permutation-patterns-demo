3ac598f75c9f38558af2beefb5b585756aa9aa06
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2023-11-18 20:35:46 +0000

    Adding Tailwind css for stying the banner
    Banner styling in progress

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 # SHProject
 
+## Viewing the Project
+
+To view the project in your browser:
+
+1. Open the Project
+
+First, install all the dependencies
+
+```bash
+npm install
+```
+
+Then, build the css file
+
+```bash
+npm run build:css
+```
+
+- If you're using a live server plugin in your code editor (such as Visual Studio Code's **Live Server**), right-click on the index.html file and select "Open with Live Server" or click Go Live button at the bottom.
+
+- Alternatively, you can simply open the index.html file in a browser to view the static content.
+
+Open http://localhost:5500 with your browser to see the result.
+
+2. Developing with Live Reload
+
+If you plan to make changes to your Tailwind styles and want to see updates in real-time, run:
+
+```bash
+npm run watch:css
+```
+
+This command will watch for changes in your Tailwind CSS file and automatically rebuild output.css whenever you make changes. You can stop watching by:
+
+```bash
+ctrl + c
+```
+
 ## License
 
 This project is licensed under the Mozilla Public License 2.0. For full license information, please see the [LICENSE](./LICENSE) file in the repository.
---
+++ component/banner.js
@@ -0,0 +1,43 @@
+const bannerConfig = {
+    logo          : 'conjure-cp',
+    title         : "Permutation Pattern",
+    author        : "Sophia Ho",
+    author_github : "https://github.com/shmy1",
+    description   : "",
+    repo_url      : 'conjure-cp/permutation-patterns-demo',
+    github        : "https://github.com/conjure-cp/permutation-patterns-demo",
+    license       : 'Mozilla Public License 2.0'
+  };
+  
+document.addEventListener('DOMContentLoaded', () => {
+    // Populate banner with config data
+    const logoElement = document.querySelector('.banner-logo-link');
+    logoElement.href = bannerConfig.author_github;
+    logoElement.textContent = bannerConfig.logo;
+  
+    const titleElement = document.querySelector('.banner-title');
+    titleElement.textContent = bannerConfig.title;
+  
+    const descriptionElement = document.querySelector('.banner-description');
+    descriptionElement.textContent = bannerConfig.description;
+  
+    const licenseElement = document.querySelector('.banner-license');
+    licenseElement.textContent = `License: ${bannerConfig.license}`;
+  
+    const githubLinks = document.querySelectorAll('.banner-github-link');
+    githubLinks.forEach(link => {
+      link.href = bannerConfig.github;
+    });
+  
+    // Fetch contributors from GitHub
+    fetch(`https://api.github.com/repos/${bannerConfig.repo_url}/contributors`)
+      .then(response => response.json())
+      .then(data => {
+        const contributorsElement = document.getElementById('contributors');
+        contributorsElement.innerHTML = data
+          .filter(contributor => contributor.login !== 'williamburns' && !contributor.login.endsWith('[bot]'))
+          .map(contributor => `<a href="https://github.com/${contributor.login}" class="mb-1 hover:underline pr-4">${contributor.login}</a>`)
+          .join('');
+      })
+      .catch(error => console.error('Error:', error));
+  });
